4b953915f76798fb61596afd50d07463ab77eba5
AstroFreakazoid <jgomezc@ucenfotec.ac.cr>
2016-10-05 17:10:22 -0600

    backup
    backup

--- CQR/CQR.UI.LOCAL/frmClientes.Designer.vb
+++ CQR/CQR.UI.LOCAL/frmClientes.Designer.vb
@@ -25,38 +25,37 @@ Partial Class frmClientes
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmClientes))
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
-        Me.TabPage2 = New System.Windows.Forms.TabPage()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.btnNuevo = New System.Windows.Forms.Button()
         Me.cbxHabilitado = New System.Windows.Forms.CheckBox()
         Me.btnRegistrar = New System.Windows.Forms.Button()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtCedula = New System.Windows.Forms.TextBox()
         Me.Nombre = New System.Windows.Forms.Label()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
-        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.txtTelefono = New System.Windows.Forms.TextBox()
+        Me.numericPremio = New System.Windows.Forms.NumericUpDown()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
-        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.numericVisitas = New System.Windows.Forms.NumericUpDown()
+        Me.txtCorreo = New System.Windows.Forms.TextBox()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.btnEliminar = New System.Windows.Forms.Button()
-        Me.btnActualizar = New System.Windows.Forms.Button()
         Me.dgClientes = New System.Windows.Forms.DataGridView()
-        Me.QrCodeImgControl1 = New Gma.QrCodeNet.Encoding.Windows.Forms.QrCodeImgControl()
-        Me.btnGuardarImagen = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
         Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.btnGuardarImagen = New System.Windows.Forms.Button()
+        Me.QrCodeImgControl1 = New Gma.QrCodeNet.Encoding.Windows.Forms.QrCodeImgControl()
         Me.TabControl1.SuspendLayout()
         Me.TabPage1.SuspendLayout()
-        Me.TabPage2.SuspendLayout()
         Me.GroupBox1.SuspendLayout()
-        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.numericPremio, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.numericVisitas, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.dgClientes, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabPage2.SuspendLayout()
         CType(Me.QrCodeImgControl1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -75,7 +74,6 @@ Partial Class frmClientes
         Me.TabPage1.Controls.Add(Me.GroupBox1)
         Me.TabPage1.Controls.Add(Me.Label1)
         Me.TabPage1.Controls.Add(Me.btnEliminar)
-        Me.TabPage1.Controls.Add(Me.btnActualizar)
         Me.TabPage1.Controls.Add(Me.dgClientes)
         Me.TabPage1.Location = New System.Drawing.Point(4, 22)
         Me.TabPage1.Name = "TabPage1"
@@ -85,36 +83,22 @@ Partial Class frmClientes
         Me.TabPage1.Text = "Clientes"
         Me.TabPage1.UseVisualStyleBackColor = True
         '
-        'TabPage2
-        '
-        Me.TabPage2.Controls.Add(Me.Button3)
-        Me.TabPage2.Controls.Add(Me.Button2)
-        Me.TabPage2.Controls.Add(Me.btnGuardarImagen)
-        Me.TabPage2.Controls.Add(Me.QrCodeImgControl1)
-        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
-        Me.TabPage2.Name = "TabPage2"
-        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(806, 423)
-        Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "Generador QR"
-        Me.TabPage2.UseVisualStyleBackColor = True
-        '
         'GroupBox1
         '
         Me.GroupBox1.Controls.Add(Me.btnNuevo)
         Me.GroupBox1.Controls.Add(Me.cbxHabilitado)
         Me.GroupBox1.Controls.Add(Me.btnRegistrar)
-        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.txtCedula)
         Me.GroupBox1.Controls.Add(Me.Nombre)
-        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.txtNombre)
         Me.GroupBox1.Controls.Add(Me.Label6)
         Me.GroupBox1.Controls.Add(Me.Label3)
         Me.GroupBox1.Controls.Add(Me.Label2)
-        Me.GroupBox1.Controls.Add(Me.TextBox3)
-        Me.GroupBox1.Controls.Add(Me.NumericUpDown2)
+        Me.GroupBox1.Controls.Add(Me.txtTelefono)
+        Me.GroupBox1.Controls.Add(Me.numericPremio)
         Me.GroupBox1.Controls.Add(Me.Label4)
-        Me.GroupBox1.Controls.Add(Me.NumericUpDown1)
-        Me.GroupBox1.Controls.Add(Me.TextBox4)
+        Me.GroupBox1.Controls.Add(Me.numericVisitas)
+        Me.GroupBox1.Controls.Add(Me.txtCorreo)
         Me.GroupBox1.Controls.Add(Me.Label5)
         Me.GroupBox1.ForeColor = System.Drawing.Color.Crimson
         Me.GroupBox1.Location = New System.Drawing.Point(29, 39)
@@ -136,6 +120,8 @@ Partial Class frmClientes
         'cbxHabilitado
         '
         Me.cbxHabilitado.AutoSize = True
+        Me.cbxHabilitado.Checked = True
+        Me.cbxHabilitado.CheckState = System.Windows.Forms.CheckState.Checked
         Me.cbxHabilitado.Location = New System.Drawing.Point(105, 229)
         Me.cbxHabilitado.Name = "cbxHabilitado"
         Me.cbxHabilitado.Size = New System.Drawing.Size(79, 17)
@@ -152,12 +138,12 @@ Partial Class frmClientes
         Me.btnRegistrar.Text = "Registrar"
         Me.btnRegistrar.UseVisualStyleBackColor = True
         '
-        'TextBox1
+        'txtCedula
         '
-        Me.TextBox1.Location = New System.Drawing.Point(82, 60)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox1.TabIndex = 5
+        Me.txtCedula.Location = New System.Drawing.Point(82, 60)
+        Me.txtCedula.Name = "txtCedula"
+        Me.txtCedula.Size = New System.Drawing.Size(100, 20)
+        Me.txtCedula.TabIndex = 5
         '
         'Nombre
         '
@@ -168,12 +154,12 @@ Partial Class frmClientes
         Me.Nombre.TabIndex = 6
         Me.Nombre.Text = "Cedula"
         '
-        'TextBox2
+        'txtNombre
         '
-        Me.TextBox2.Location = New System.Drawing.Point(82, 86)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox2.TabIndex = 7
+        Me.txtNombre.Location = New System.Drawing.Point(82, 86)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(100, 20)
+        Me.txtNombre.TabIndex = 7
         '
         'Label6
         '
@@ -202,19 +188,19 @@ Partial Class frmClientes
         Me.Label2.TabIndex = 15
         Me.Label2.Text = "Visitas"
         '
-        'TextBox3
+        'txtTelefono
         '
-        Me.TextBox3.Location = New System.Drawing.Point(82, 112)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox3.TabIndex = 9
+        Me.txtTelefono.Location = New System.Drawing.Point(82, 112)
+        Me.txtTelefono.Name = "txtTelefono"
+        Me.txtTelefono.Size = New System.Drawing.Size(100, 20)
+        Me.txtTelefono.TabIndex = 9
         '
-        'NumericUpDown2
+        'numericPremio
         '
-        Me.NumericUpDown2.Location = New System.Drawing.Point(105, 190)
-        Me.NumericUpDown2.Name = "NumericUpDown2"
-        Me.NumericUpDown2.Size = New System.Drawing.Size(77, 20)
-        Me.NumericUpDown2.TabIndex = 14
+        Me.numericPremio.Location = New System.Drawing.Point(105, 190)
+        Me.numericPremio.Name = "numericPremio"
+        Me.numericPremio.Size = New System.Drawing.Size(77, 20)
+        Me.numericPremio.TabIndex = 14
         '
         'Label4
         '
@@ -225,19 +211,19 @@ Partial Class frmClientes
         Me.Label4.TabIndex = 10
         Me.Label4.Text = "Telefono"
         '
-        'NumericUpDown1
+        'numericVisitas
         '
-        Me.NumericUpDown1.Location = New System.Drawing.Point(105, 164)
-        Me.NumericUpDown1.Name = "NumericUpDown1"
-        Me.NumericUpDown1.Size = New System.Drawing.Size(77, 20)
-        Me.NumericUpDown1.TabIndex = 13
+        Me.numericVisitas.Location = New System.Drawing.Point(105, 164)
+        Me.numericVisitas.Name = "numericVisitas"
+        Me.numericVisitas.Size = New System.Drawing.Size(77, 20)
+        Me.numericVisitas.TabIndex = 13
         '
-        'TextBox4
+        'txtCorreo
         '
-        Me.TextBox4.Location = New System.Drawing.Point(82, 138)
-        Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
-        Me.TextBox4.TabIndex = 11
+        Me.txtCorreo.Location = New System.Drawing.Point(82, 138)
+        Me.txtCorreo.Name = "txtCorreo"
+        Me.txtCorreo.Size = New System.Drawing.Size(100, 20)
+        Me.txtCorreo.TabIndex = 11
         '
         'Label5
         '
@@ -260,30 +246,63 @@ Partial Class frmClientes
         '
         'btnEliminar
         '
-        Me.btnEliminar.Location = New System.Drawing.Point(610, 334)
+        Me.btnEliminar.Location = New System.Drawing.Point(692, 334)
         Me.btnEliminar.Name = "btnEliminar"
         Me.btnEliminar.Size = New System.Drawing.Size(75, 23)
         Me.btnEliminar.TabIndex = 24
         Me.btnEliminar.Text = "Eliminar"
         Me.btnEliminar.UseVisualStyleBackColor = True
         '
-        'btnActualizar
-        '
-        Me.btnActualizar.Location = New System.Drawing.Point(691, 334)
-        Me.btnActualizar.Name = "btnActualizar"
-        Me.btnActualizar.Size = New System.Drawing.Size(75, 23)
-        Me.btnActualizar.TabIndex = 23
-        Me.btnActualizar.Text = "Actualizar"
-        Me.btnActualizar.UseVisualStyleBackColor = True
-        '
         'dgClientes
         '
-        Me.dgClientes.BackgroundColor = System.Drawing.Color.Sienna
+        Me.dgClientes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.dgClientes.BackgroundColor = System.Drawing.Color.Silver
         Me.dgClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.dgClientes.Location = New System.Drawing.Point(301, 72)
         Me.dgClientes.Name = "dgClientes"
         Me.dgClientes.Size = New System.Drawing.Size(476, 256)
         Me.dgClientes.TabIndex = 22
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.Button3)
+        Me.TabPage2.Controls.Add(Me.Button2)
+        Me.TabPage2.Controls.Add(Me.btnGuardarImagen)
+        Me.TabPage2.Controls.Add(Me.QrCodeImgControl1)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(806, 423)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Generador QR"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(401, 82)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Generador"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(299, 291)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(177, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'btnGuardarImagen
+        '
+        Me.btnGuardarImagen.Location = New System.Drawing.Point(299, 262)
+        Me.btnGuardarImagen.Name = "btnGuardarImagen"
+        Me.btnGuardarImagen.Size = New System.Drawing.Size(177, 23)
+        Me.btnGuardarImagen.TabIndex = 2
+        Me.btnGuardarImagen.Text = "Guardar Imagen"
+        Me.btnGuardarImagen.UseVisualStyleBackColor = True
         '
         'QrCodeImgControl1
         '
@@ -298,33 +317,6 @@ Partial Class frmClientes
         Me.QrCodeImgControl1.TabStop = False
         Me.QrCodeImgControl1.Text = "QrCodeImgControl1"
         '
-        'btnGuardarImagen
-        '
-        Me.btnGuardarImagen.Location = New System.Drawing.Point(299, 262)
-        Me.btnGuardarImagen.Name = "btnGuardarImagen"
-        Me.btnGuardarImagen.Size = New System.Drawing.Size(177, 23)
-        Me.btnGuardarImagen.TabIndex = 2
-        Me.btnGuardarImagen.Text = "Guardar Imagen"
-        Me.btnGuardarImagen.UseVisualStyleBackColor = True
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(299, 291)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(177, 23)
-        Me.Button2.TabIndex = 3
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
-        '
-        'Button3
-        '
-        Me.Button3.Location = New System.Drawing.Point(401, 82)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(75, 23)
-        Me.Button3.TabIndex = 4
-        Me.Button3.Text = "Generador"
-        Me.Button3.UseVisualStyleBackColor = True
-        '
         'frmClientes
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -338,12 +330,12 @@ Partial Class frmClientes
         Me.TabControl1.ResumeLayout(False)
         Me.TabPage1.ResumeLayout(False)
         Me.TabPage1.PerformLayout()
-        Me.TabPage2.ResumeLayout(False)
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
-        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.numericPremio, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.numericVisitas, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.dgClientes, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabPage2.ResumeLayout(False)
         CType(Me.QrCodeImgControl1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
@@ -355,21 +347,20 @@ Partial Class frmClientes
     Friend WithEvents btnNuevo As Button
     Friend WithEvents cbxHabilitado As CheckBox
     Friend WithEvents btnRegistrar As Button
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents txtCedula As TextBox
     Friend WithEvents Nombre As Label
-    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents txtNombre As TextBox
     Friend WithEvents Label6 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label2 As Label
-    Friend WithEvents TextBox3 As TextBox
-    Friend WithEvents NumericUpDown2 As NumericUpDown
+    Friend WithEvents txtTelefono As TextBox
+    Friend WithEvents numericPremio As NumericUpDown
     Friend WithEvents Label4 As Label
-    Friend WithEvents NumericUpDown1 As NumericUpDown
-    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents numericVisitas As NumericUpDown
+    Friend WithEvents txtCorreo As TextBox
     Friend WithEvents Label5 As Label
     Friend WithEvents Label1 As Label
     Friend WithEvents btnEliminar As Button
-    Friend WithEvents btnActualizar As Button
     Friend WithEvents dgClientes As DataGridView
     Friend WithEvents TabPage2 As TabPage
     Friend WithEvents Button3 As Button
